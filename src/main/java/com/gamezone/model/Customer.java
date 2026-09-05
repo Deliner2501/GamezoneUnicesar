56e09266706837111915026ff2c113c1ff8b5d01
Santiago Meza <smmeza@unicesar.edu.co>
2026-09-05 16:10:39 -0500

    feat: implement Customer subclass with purchase history

--- src/main/java/com/gamezone/model/Customer.java
+++ src/main/java/com/gamezone/model/Customer.java
@@ -1,7 +1,32 @@
-
 package com.gamezone.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class Customer {
-    
+/**
+ * Represents a customer of the store.
+ * A customer can purchase products and has a purchase history.
+ */
+public class Customer extends Person {
+
+    private String email;
+    private List<Sale> purchaseHistory;
+
+    public Customer(String name, String id, String phone, String email) {
+        super(name, id, phone);
+        this.email = email;
+        this.purchaseHistory = new ArrayList<>();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<Sale> getPurchaseHistory() {
+        return purchaseHistory;
+    }
+
+    public void addPurchase(Sale sale) {
+        this.purchaseHistory.add(sale);
+    }
 }
