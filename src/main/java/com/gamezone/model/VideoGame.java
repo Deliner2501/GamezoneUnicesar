93173c2cc2849b52d608d1dae9ce9a0259a7da14
Ronald Yessid Mendoza Fernandez <rymendoza@unicesar.edu.co>
2026-09-05 16:35:20 -0500

    feat: add VideoGame class extending Product with description logic

--- src/main/java/com/gamezone/model/VideoGame.java
+++ src/main/java/com/gamezone/model/VideoGame.java
@@ -1,7 +1,33 @@
-
 package com.gamezone.model;
 
+/**
+ * Represents a video game product, characterized by platform,
+ * genre and age rating.
+ */
+public class VideoGame extends Product {
 
-public class VideoGame {
-    
+    private String platform;
+    private String genre;
+    private String ageRating;
+
+    public VideoGame(String id, String title, double price, int stock,
+                      String platform, String genre, String ageRating) {
+        super(id, title, price, stock);
+        this.platform = platform;
+        this.genre = genre;
+        this.ageRating = ageRating;
+    }
+
+    /**
+     * Returns a full description integrating this video game's
+     * specific characteristics.
+     *
+     * @return the full description of the video game
+     */
+    @Override
+    public String getFullDescription() {
+        return getTitle() + " - Platform: " + platform + ", Genre: " + genre
+                + ", Age rating: " + ageRating + ", Price: " + getPrice()
+                + ", Stock: " + getStock();
+    }
 }
